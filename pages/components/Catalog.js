6bef2050d31c98360ff2fbc562b7ab01fbcffd2d
ajiaron <aaronjiang2001@gmail.com>
2024-01-08 18:57:23 -0800

    more ui fixes

--- pages/components/Catalog.js
+++ pages/components/Catalog.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Image from 'next/image'
 import styles from '../../styles/Catalog.module.scss'
+import dashbill from '../../public/assets/dashbill-catalog3.png'
+import peakingduck from '../../public/assets/peakingduck-catalog.svg'
+import milestone from '../../public/assets/milestone-catalog3.jpg'
+import jennie from '../../public/assets/jennie2.jpg'
+import atmosphere from '../../public/assets/atmosphere.jpg'
 import Button from './Button';
 import {motion, AnimatePresence, useScroll,useMotionValueEvent, useAnimation, inView} from 'framer-motion'
 
@@ -15,11 +21,15 @@ const Catalog = ({scrollY, screenWidth}) => {
              }}
              className={styles['catalog-content-container']}>
                 <div className={styles["catalog-content-item"]}/>
+               
                 <div className={styles["catalog-content-item"]}/>
+              
                 <div className={styles["catalog-content-item"]}/>
+               
                 <div className={styles["catalog-content-item"]}/>
+             
                 <div className={styles["catalog-content-item"]}/>
-                
+               
             </motion.div>
           
         </div>
